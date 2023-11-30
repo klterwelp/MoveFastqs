6d1f33dcaaea893b814ec3035b058fd9ed76ac36
klterwelp <klterwelp@gmail.com>
2023-11-30 12:25:34 -0500

    Add check for checksums prior to script start

--- movefiles.sh
+++ movefiles.sh
@@ -39,6 +39,120 @@ exec &>> "{$data_dir}"/log/movefiles_"${current_time}".log
 
 # CHECKING CHECKSUMS ------------------------------------------------- #
 
+# if previous output folder exists, delete it 
+echo -e "checking for old folders, will remove to rerun analysis if md5sums are not identical" 
+
+if [ -d "$outputChk" ]
+then 
+    echo -e "Previous output folder exists. Verifying existing checksum file. " 
+    current_time=$(date "+%Y%m%d_%H%M%S")
+    md5sum "${data_dir}/"*.fastq.gz > "${outputChk}/dwnld_${current_time}.checksum"
+        if cmp -s "${outputChk}/dwnld_${current_time}.checksum" "${outputChk}/"dwnld.checksum; then
+            echo "The md5sum files are identical."
+            if cmp -s "$checksumFile" "${outputChk}/seq.checksum"; then 
+                echo "The sequencing checksum files are also identicial. No need to continue script."
+                exit 0 
+            else
+                echo "The sequencing checksum files are not identical. Need to test checksums again. Deleting old output folders"
+                rm -Rfv -- "$outputChk"
+            fi
+        else
+            echo "The md5sum files are not identical. Need to generate new md5sums. Deleting old output folders"
+            echo "Listing non-matching lines..."
+            grep -Fxvf "${outputChk}/dwnld_${current_time}.checksum" "${outputChk}/"dwnld.checksum
+            rm -Rfv -- "$outputChk"
+        fi
+fi 
+    # -R deletes recursively, -f ignore non-existant files, -v verbose
+    # '--'' : no more flags for rm command 
+
+# making new import folders 
+echo -e "creating new output folders" 
+mkdir -p "${outputChk}"
+
+# Does sequencing checksum file exist? 
+echo "Sequencing checksum text file: $checksumFile"
+if [ ! -f "$checksumFile" ]; then
+    echo "Error: File $checksumFile not found. Exiting script."
+    exit 1
+    else
+        echo "File: $checksumFile found. Copying to output folder"
+        cp "$checksumFile" "${outputChk}/seq.checksum"
+fi
+
+# Make declarative array to hold sequencing checksum information
+declare -A seqChecksumsArray
+
+echo "Making declarative array to hold sequencing checksum info..."
+
+while IFS=' ' read -r filename checksum; do
+    seqChecksumsArray["$filename"]=$checksum
+done < "$checksumFile"
+
+# Make checksum array for downloaded fastq.gz files 
+echo "Generate downloaded fastq file checksums"
+
+md5sum "${data_dir}/"*.fastq.gz > "${outputChk}/"dwnld.checksum
+
+echo "Making declarative array to hold downloaded checksum info..."
+
+declare -A dwnlChecksumsArray 
+
+while IFS=' ' read -r checksum fullpath; do
+    filename=$(basename "$fullpath")
+    dwnlChecksumsArray["$filename"]=$checksum
+    done < "${outputChk}/dwnld.checksum"
+
+# Test that filenames match 
+echo "Identifying any files not downloaded..."
+
+for filename in "${!seqChecksumsArray[@]}"; do
+    dwnlChk=${dwnlChecksumsArray["$filename"]}
+
+    if [ -z "$dwnlChk" ]; then 
+        match="FALSE"
+        echo "$filename" >> "${outputChk}/"not_downloaded.txt 
+        else
+        match="TRUE"
+        echo "$filename" >> "${outputChk}/"downloaded.txt 
+    fi
+echo "$filename $match" >> "${outputChk}/"files.checksum
+done
+notdwl=$(wc -l < "${outputChk}/"not_downloaded.txt)
+dwl=$(wc -l < "${outputChk}/"downloaded.txt)
+
+echo "There are $notdwl files that were not downloaded but exist as files in seq checksum 
+List of files in not_downloaded.txt"
+echo "There are $dwl files that were downloaded AND exist as files in seq checksum. 
+List of files in downloaded.txt"
+
+echo "$dwl/${#dwnlChecksumsArray[@]} downloaded files exist within the sequencing checksum"
+
+# Test that checksums match
+echo "Testing checksums for matches between seq and downloaded checksums..."
+
+for filename in "${!dwnlChecksumsArray[@]}"; do
+    dwnlChk=${dwnlChecksumsArray["$filename"]}
+    sqtChk=${seqChecksumsArray["$filename"]}
+
+    if [ -z "$sqtChk" ]; then 
+       match="not in seq.checksum"
+       else 
+        if [[ "$sqtChk" == "$dwnlChk" ]]; then
+        match="TRUE"
+        else
+        match="FALSE"
+        fi
+    fi
+    echo "$filename $match" >> "${outputChk}/"match.checksum
+    done
+
+numTrue=$(grep -c "TRUE" "${outputChk}/"match.checksum)
+numFalse=$(grep -c "FALSE" "${outputChk}/"match.checksum)
+echo "There are $numTrue fastq that match seq checksums.
+There are $numFalse fastq that do NOT match seq checksums."
+
+# MOVING FILES ---------------------------- # 
 declare -A FsamplePathArray
 declare -A RsamplePathArray
 # start declarative arrays that will hold the sample id and path information depending on R1 or R2
